9fcbeb8b07d6a6a0fd72a426413ed4c1b9ab744d
ASK <lucykenos@icloud.com>
2023-06-05 12:33:49 +0930

    Cleaned Code

--- Fundamentals.py
+++ Fundamentals.py
@@ -1,4 +1,4 @@
-#Variables
+#VARIABLES
 age = 20 #an int
 age = 21.0 #a floating interger
 first_name = "John" #a string
@@ -10,30 +10,28 @@ is_new = True #a boolean
             -Which itself is stored somewhere on the computer's memory.
 """
 
-#Variable Type Convertors
-str() #turns the variable called upon or the value within the paranthesis into a string
+#VARIABLE COVERTORS
+str() #turns the variable/value within the parenthesis into a string
 
-int() #turns the variable called upon or the value within the paranthesis into an interger
+int() #turns the variable/value within the parenthesis into an interger
 
-float() #turns the variable called upon or the value within the paranthesis into a floating interger; for examples 3 becomes 3.0
+float() #turns the variable/value within the parenthesis into a floating interger; for examples 3 becomes 3.0
 
-bool() #turns the value within the paranthesis into a boolean
+bool() #turns the variable/value within the paranthesis into a boolean
 
 #Output & Input
 print("Hello World!") #a function which prints the result of all items within the parenthesis
-
 first_name = input("What is your name?\n") #a function which takes the result of prompting a user for an input and returns the value as a string. -In this case, it is given immediately to the variable 'first_name'.
-
 print("Hello " + first_name.capitalize()) #concatenates (joins the strings) and prints the result
 
-#Calculator Program
+#ADDITION PROGRAM
 first_number = float(input("First#: "))
 second_number = float(input("Second#: "))
 sum = first_number + second_number
 sum = str(sum)
 print("Sum: " + sum)
 
-#If & Else Statements
+#IF & ELSE STATEMENTS
 """
 name = input("Say my name!\n")
 
@@ -42,10 +40,10 @@ if name.lower() == "heisenberg":
 else:
 	print("Fuck off! I'm the one whoo knocks!")
 
-The ‘if’ and ‘else’ statement which can be used to make simple, conditional triggers for the compiler to run specific codes.
+The 'if' and 'else' statement which can be used to make simple, conditional triggers for the compiler to run specific codes. If there's a need for more conditions, you can also use 'elif' statements.
 """
 
-#While Loop
+#WHILE LOOP
 name = input("Say my name!\n")
 while name.lower() != "heisenberg":
     print("Fuck off! I'm the one who knocks!")
@@ -53,7 +51,7 @@ while name.lower() != "heisenberg":
 print("You're goddamn right!")
 #The ‘while’ statement which can be used to loop a certain set of conditional code until a specific result is achieved. -In this instance, the while loop prompts the user until the correct name is entered.
 
-#String Functions
+#STRING FUNCTIONS
 example1 = "String"
 
 example1.upper() #turns the entire string uppercase; does not modify the original string, only returns a new string
@@ -73,24 +71,24 @@ example1.title() #capitalizes the first letter of each word, while the rest rema
         -The index of a string object begins with 0 being the first letter. And counts backwards from -1.
 """
 
-#Example Codes
+#EXAMPLE CODES
 example1 = "String Test"
 print(example1.lower()) #=> "string test"
 print(example1)
 print(example1.upper()) #=> "STRING TEST"
 print(example1)
-print(example1.capitalize()) #=> "String test
+print(example1.capitalize()) #=> "String test"
 print(example1)
 print(example1.title()) #=> "String Test"
 
 x = "-".join(["Coding", "is", "awesome"])
-print(x)
+print(x) #=> "Coding-is-awesome"
 x = " ".join(["Coding", "is", "awsome."])
-print(x)
+print(x) #=> "Coding is awesome."
 
 example1 = "Python for Beginners"
-print(example1.find("y")) #this will return a 1
+print(example1.find("y")) #this will return a 1; which is the index of the first occurance of 'y'
 print(example1.replace("for", "4")) #=> 'Python 4 Beginners'
-print(example1)
+print(example1) #this will return the original string; as the original string was not modified
 
 print("Python" in example1) #this checks to see if we have the word 'Python' in the variable 'example1'; this returns the boolean value 'True'
